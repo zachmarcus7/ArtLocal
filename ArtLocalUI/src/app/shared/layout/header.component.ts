3375025cd274f4a917e70dd4cccc85dfd5ad98c0
Zach Marcus <55953922+zachmarcus7@users.noreply.github.com>
2022-07-03 18:12:35 -0700

    Delete ArtLocalUI/src/app directory

--- ArtLocalUI/src/app/shared/layout/header.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, OnInit } from '@angular/core';
- 
-@Component({
-  selector: 'app-header',
-  templateUrl: `./header.component.html`,
-  styleUrls: []
-})
-export class HeaderComponent {
-}
