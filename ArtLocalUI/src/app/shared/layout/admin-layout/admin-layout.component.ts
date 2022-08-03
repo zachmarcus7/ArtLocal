3a1f4a528ed824727581ef48140badebaa58f926
Zach Marcus <55953922+zachmarcus7@users.noreply.github.com>
2022-08-03 13:05:17 -0700

    Add files via upload

--- ArtLocalUI/src/app/shared/layout/admin-layout/admin-layout.component.ts
+++ ArtLocalUI/src/app/shared/layout/admin-layout/admin-layout.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, HostListener, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, HostListener, Output, EventEmitter, ViewChild } from '@angular/core';
 import { AuthService } from 'src/app/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-admin-layout',
@@ -13,12 +14,21 @@ export class AdminLayoutComponent implements OnInit {
   displayLogo: boolean;
   sidenavOpened: boolean;
   showTitles: boolean;
+  minWidth: string;
+  regWidth: string;
+  sidenavWidth: string;
+  contentMargin: number;
+  @ViewChild('sidenav') sidenav!: MatSidenav;
 
   constructor(private authService: AuthService, private router: Router) {
+    this.minWidth = "65px";
+    this.regWidth = "260px";
+    this.contentMargin = 260;
     this.adminLoggedIn = false;
     this.displayLogo = this.isLargeScreen() ? true : false;
     this.sidenavOpened = this.isLargeScreen() ? true : false;  
     this.showTitles = this.isLargeScreen() ? true : false;  
+    this.sidenavWidth = this.isLargeScreen() ? this.regWidth : this.minWidth;
   }
 
   ngOnInit() {
@@ -37,6 +47,35 @@ export class AdminLayoutComponent implements OnInit {
         return true;
     } else {
         return false;
+    }
+  }
+
+  setSidenav() {
+    if (this.isLargeScreen()) {
+      this.showTitles = !this.showTitles;
+
+      // check if the sidenav was closed and user went back to full screen
+      if (!this.sidenav.opened) {
+        this.sidenavWidth = this.regWidth;
+        this.sidenav.toggle();
+        this.showTitles = !this.showTitles;
+      }
+      else {
+        this.sidenavWidth = this.sidenavWidth == this.regWidth ? this.minWidth : this.regWidth;
+        this.contentMargin = this.contentMargin == 260 ? 65 : 260;
+      }
+    }
+    else {
+      this.sidenav.toggle();
+    }
+  }
+
+  checkSidenav() {
+    // this is for when a user clicks on the sidenav in full screen when it's at the min width
+    if (this.sidenavWidth == this.minWidth) {
+      this.showTitles = !this.showTitles;
+      this.sidenavWidth = this.regWidth;
+      this.contentMargin = this.contentMargin == 260 ? 65 : 260;
     }
   }
 
