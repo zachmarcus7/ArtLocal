8f4fac0fedac08899d3604e95f2ae6086ee00409
Zach Marcus <55953922+zachmarcus7@users.noreply.github.com>
2022-08-16 20:09:16 -0700

    Add files via upload

--- ArtLocalUI/src/app/home/home.module.ts
+++ ArtLocalUI/src/app/home/home.module.ts
@@ -12,6 +12,17 @@ import { FormExampleComponent } from './pages/form-example/form-example.componen
 import { FormOneComponent } from './pages/form-one/form-one.component';
 import { FormTwoComponent } from './pages/form-two/form-two.component';
 
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+
+
 
 @NgModule({
   imports: [
@@ -19,7 +30,16 @@ import { FormTwoComponent } from './pages/form-two/form-two.component';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    RouterModule
+    RouterModule,
+    MatSlideToggleModule,
+    MatProgressBarModule,
+    MatTableModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule
   ],
   declarations: [ 
     HomeComponent, 
@@ -27,7 +47,9 @@ import { FormTwoComponent } from './pages/form-two/form-two.component';
     CheckoutComponent,
     CustomerRegisterComponent, 
     CustomerLoginComponent, 
-    FormExampleComponent, FormOneComponent, FormTwoComponent
+    FormExampleComponent, 
+    FormOneComponent, 
+    FormTwoComponent
   ],
   exports: [
     CommonModule,
